83284a2e9c18ab54ec226da31d5b4be3412a6ba4
Carlos7678 <carloscontacto4@gmail.com>
2024-02-17 18:49:45 +0100

    ej4

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -1,8 +1,6 @@
 @extends('plantilla')
 
-
 @section('titulo', 'Listado posts')
-
 
 @section('contenido')
     <h1>Listado de posts</h1>
@@ -13,22 +11,25 @@
             <button type="submit" class="btn btn-primary">Crear Posts Automáticos</button>
         </form>
 
-
         <!-- Listado de posts -->
         @forelse($posts as $post)
             <div>
                 <h3>{{ $post->titulo }} (<em>{{ $post->usuario->login }}</em>)
-                    <button class="btn btn-warning" onclick="window.location='{{ route('posts.show', $post->id) }}'">Ver</button>
-                    <form method="POST" action="{{route('posts.destroy', $post->id)}}" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger" onclick="window.location='{{ route('posts.destroy', $post->id) }}'">Borrar</button>
-                    </form>
+                <button class="btn btn-warning" onclick="window.location='{{ route('posts.show', $post->id) }}'">Ver</button>
+                <form method="POST" action="{{ route('posts.destroy', $post->id) }}" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger">Borrar</button>
+                </form>
+                <!-- Formulario para editar -->
+                <form method="POST" action="{{ route('posts.editarPrueba', $post->id) }}" style="display:inline;">
+                    @csrf
+                    <button type="submit" class="btn btn-success">Editar</button>
+                </form></h3>
             </div>
         @empty
             <p>No hay posts</p>
         @endforelse
     </div>
 @endsection
-
 
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -8,11 +8,9 @@ Route::get('/', function () {
     return view('posts.inicio', ['fechaActual' => DateHelper::fechaActual()]);
 })->name('inicio');
 
-
 Route::get('/posts', function () {
     return view('posts.listado');
 })->name('posts_listado');
-
 
 Route::get('/posts/{id}', function ($id) {
     return view('posts.show')->with(['id' => $id]);
@@ -20,16 +18,15 @@ Route::get('/posts/{id}', function ($id) {
 
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 
-Route::resource('posts', PostController::class)->only([
-    'index', 'show', 'create', 'edit'
-]);
-
-
-
 Route::post('/posts/nuevoPrueba', [PostController::class, 'nuevoPrueba'])->name('posts.nuevoPrueba');
-Route::get('/posts/editarPrueba/{id}', 'App\Http\Controllers\PostController@editarPrueba');
+
+Route::post('/posts/editar/{id}', [PostController::class, 'editarPrueba'])->name('posts.editarPrueba');
+
+Route::get('/posts/editarPrueba/{id}', [PostController::class, 'editarPrueba']);
+
 Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
 
+Route::resource('posts', PostController::class)->only(['index', 'show', 'create', 'edit']);
 
 
 
